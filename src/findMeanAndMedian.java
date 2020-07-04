f56e9b97aa687005937584bb050800d4b3d1ac5c
kevin Paredes <kparede1@mail.sfsu.edu>
2020-07-03 23:05:24 -0700

    Added Array rotation problem

--- src/findMeanAndMedian.java
+++ src/findMeanAndMedian.java
@@ -1,58 +1,143 @@
 public class findMeanAndMedian {
 
-    public static void main(String[] args) {
+    public static void sort(int[] arr) {
 
-        int [] arr = {2, 3, 4, 2, 3, 6, 8, 4, 5};
-        int d = 5;
         int n = arr.length;
 
-        int temp ;
+        for (int i = 0; i < n; i++) {
 
-        int index ;
+            int currentMin = arr[i];
+            int currentIndex = i;
 
-        int median;
+            for (int j = i + 1; j < n; j++) {
 
-        int count = 0;
+                if (currentMin > arr[j]) {
 
+                    currentMin = arr[j];
+                    currentIndex = j;
 
-        while (d < n) {
-
-            for (int j = 0; j < d ; j++) {
-
-                    temp = arr[j];
-                    index = j;
-
-                for (int k = 1; k < d ; k++) {
-
-                    if (temp > arr[k]){
-                        temp = arr[k];
-                        index = k;
-                    }
                 }
 
-                if (index != j){
-                    arr[index]= arr[j];
-                    arr [j] = temp;
-                }
+            }
+            if (currentIndex != i) {
+
+                arr[currentIndex] = arr[i];
+                arr[i] = currentMin;
             }
 
-            if (d % 2 != 0){
+        }
+    }
 
-                median = arr[d /2];
+    public static int median (int [] arr , int d){
+
+        int median = 0;
+
+        sort(arr);
+
+
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+        while (d < arr.length) {
+            if (d % 2 == 0) {
+
+                median = arr[(d - 1) / 2];
+                System.out.println();
+                System.out.println(median);
+            } else {
+                median = arr[d / 2];
+                System.out.println();
+                System.out.println(median);
             }
-            else {
-                median = arr[(d  - 1) / 2];
-            }
-
-            if (d + 1 > 2 * median){
-
-                System.out.println(d +" > "+ 2*median);
-                count++;
-            }
-
             d++;
         }
 
-        System.out.println(count);
+        return median;
     }
+
+    public static int notification ( int [] arr, int d){
+
+        int notification = 0;
+
+        for (int i = d; i < arr.length; i++) {
+
+            System.out.println("This is the next value " + arr[i] + " " +" this is the median "+ median(arr, d) *2);
+            if (arr[i] > median(arr, d ) * 2){
+                System.out.print(arr[i] +" ");
+                notification++;
+                System.out.println("added 1 not");
+                d = d+1;
+            }
+        }
+        return notification;
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] arr = {2, 3, 4, 2, 3, 6, 8, 4, 5};
+        //int[] arr = {10, 20, 30, 40, 50};
+        int d = 5;
+        //int d = 3;
+
+        System.out.println(" This is the notification " + notification(arr, d));
+
+//        int n = arr.length;
+
+//        int temp;
+//
+//        int index;
+//
+//        int median;
+//
+//        int count = 0;
+
+
+//        while (d < n) {
+//
+//            for (int j = 0; j < d; j++) {
+//
+//                temp = arr[j];
+//                index = j;
+//
+//                for (int k = 1; k < d; k++) {
+//
+//                    if (temp > arr[k]) {
+//                        temp = arr[k];
+//                        index = k;
+//                    }
+//                }
+//
+//                if (index != j) {
+//                    arr[index] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
+//
+//            for (int z = 0; z < n; z++) {
+//                System.out.print(arr[z] + " ");
+//
+//            }
+//
+//            if (d % 2 != 0) {
+//
+//                median = arr[d / 2];
+//                System.out.println(median);
+//            } else {
+//                median = arr[(d - 1) / 2];
+//                System.out.println(median);
+//            }
+//
+//            if (d + 1 > 2 * median) {
+//
+//                System.out.println(d + " > " + 2 * median);
+//                count++;
+//            }
+//
+//            d++;
+//        }
+//
+//        System.out.println(count);
+   }
 }
---
+++ src/hackerRankInterviewQuestions/arrays/solutionArrayLeftRotationProblem.java
@@ -0,0 +1,85 @@
+package hackerRankInterviewQuestions.arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class solutionArrayLeftRotationProblem {
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+
+
+        for ( int i = 0; i < d; i++){
+
+            leftR(a);
+        }
+        return a;
+    }
+
+    static void leftR(int [] a){
+
+        int i, temp;
+        temp = a[0];
+
+        for(i = 0; i < a.length -1; i++){
+            a[i]= a[i+1];
+        }
+        a[i]=temp;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+/*
+
+Input (stdin)
+
+5 4
+1 2 3 4 5
+
+===================
+Expected Output
+
+5 1 2 3 4
+
+ */
